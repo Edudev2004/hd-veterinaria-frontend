626d390a0c48ea32cbcf5370d3ae850ea17b6152
FabrizioMn <fabriziomn3@gmail.com>
2026-09-14 17:24:43 -0500

    Agregando hook personalizado useSteeperContext

--- src/features/appointments/context/AppointmentStepperContext.tsx
+++ src/features/appointments/context/AppointmentStepperContext.tsx
@@ -1,3 +1,9 @@
 import { createContext, useContext } from "react";
 
 const AppointmentStepperContext = createContext(undefined);
+
+
+//Hook perzonalizado
+export const useStepperContext = () => {
+  return useContext(AppointmentStepperContext);
+};
